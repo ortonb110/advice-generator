de7c590790b22879eba93bf041852b1cc70dcb98
Orton Bright <ortonb110@gmail.com>
2023-02-27 23:07:59 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryLightCyan: "hsl(193, 38%, 86%)",
+        primaryNeonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      }
+    },
+  },
+  plugins: [],
+}
